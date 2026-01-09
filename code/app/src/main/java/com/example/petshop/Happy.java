52dd3bda045921d72bfed2075d5750b9084b308f
Kito Lee Son <109246782+kitoleeson@users.noreply.github.com>
2026-01-09 13:16:20 -0700

    adding spaces

--- code/app/src/main/java/com/example/petshop/Happy.java
+++ code/app/src/main/java/com/example/petshop/Happy.java
@@ -7,6 +7,7 @@ public class Happy extends Mood {
     public Happy(){ super(); }
 
     public Happy(Date date){ super(date); }
+    
     @Override
     public String getMood(){ return "happy"; }
 
